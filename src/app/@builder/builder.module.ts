3be44c8c45f1263054fda6b75f92326396a0d2a6
Mauricio Martinez <n_othing@hotmail.com>
2019-07-24 17:39:59 -0500

    First commit with shell and graph experiment

---
+++ src/app/@builder/builder.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { BuilderComponent } from './builder.component';
+
+const COMPONENTS = [
+  BuilderComponent,
+];
+
+@NgModule({
+  declarations: [...COMPONENTS],
+  imports: [],
+  providers: [],
+  exports: [...COMPONENTS]
+})
+export class BuilderModule { }
